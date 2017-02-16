19fa672663d9d85e39a9fe3200f9d08c2a9fd6f5
Filip Hracek <filip.hracek@gmail.com>
2017-02-15 21:14:39 -0800

    Add SessionResult class
    This class encapsulates result of the whole slot_machine session (including reroll).

--- lib/result.dart
+++ lib/result.dart
@@ -14,3 +14,24 @@ enum Result {
   /// Major failure.
   criticalFailure
 }
+
+/// The final result of a slot machine session.
+///
+/// This encapsulates the last [Result] as well as whether or not the session
+/// had any re-roll ([wasRerolled]).
+class SessionResult {
+  /// The final result of the roll.
+  final Result result;
+
+  /// Whether or not the user rerolled after first initial failure.
+  final bool wasRerolled;
+
+  /// Creates a RollResult with information about rerolling.
+  const SessionResult(this.result, this.wasRerolled);
+
+  /// Whether the session ended with success (but not critical success).
+  bool get isSuccess => result == Result.success;
+
+  @override
+  String toString() => "SessionResult<$result,wasRerolled=$wasRerolled>";
+}
